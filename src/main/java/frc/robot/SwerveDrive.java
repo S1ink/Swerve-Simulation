c4823930bafb1cabc5f4e416e58a9ca04634e0c4
Sam Richter <hoodie888@outlook.com>
2023-06-27 20:31:51 -0500

    Begin class structures
    Co-authored-by: sackihz <sackihz@users.noreply.github.com>
    Co-authored-by: yeet282 <yeet282@users.noreply.github.com>
    Co-authored-by: beardedone55 <beardedone55@users.noreply.github.com>
    Co-authored-by: Yushiway <Yushiway@users.noreply.github.com>

---
+++ src/main/java/frc/robot/SwerveDrive.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import frc.robot.team3407.drive.Types.*;
+
+
+public class SwerveDrive {
+
+	public static interface SwerveModule {
+
+
+
+	}
+
+	public static class SwerveConfig {
+
+	}
+
+
+
+}
